d5da282568550c065abf75f6fe0e59f78f5f6f97
JuanmaVD <JuanmaVD21@gmail.com>
2024-06-06 10:23:07 -0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String nombre = JOptionPane.showInputDialog("Ingrese su nombre");
+
+        String apellido = JOptionPane.showInputDialog("Ingrese su apellido");
+
+        String usuario = JOptionPane.showInputDialog("Ingrese su usuario");
+
+        String contrasenia = JOptionPane.showInputDialog("Ingrese su contraseña");
+
+        String telefono = JOptionPane.showInputDialog("Ingrese su telefono");
+
+        String mail = JOptionPane.showInputDialog("Ingrese su mail");
+
+        Usuario usuario1 = new Usuario(usuario, contrasenia, nombre, apellido, telefono, mail);
+        JOptionPane.showMessageDialog(null, "Usuario creado: \nnombre: " + usuario1.getNombre() + "\napellido: "
+        + usuario1.getApellido() + "\nusuario: " + usuario1.getUsuario() + "\ncontraseña: " + usuario1.getContrasenia() + "\ntelefono: " +
+                usuario1.getNumeroTelefono() + "\nmail: " + usuario1.getMail());
     }
 }
---
+++ src/Usuario.java
@@ -0,0 +1,64 @@
+public class Usuario {
+    private String usuario;
+    private String contrasenia;
+    private String nombre;
+    private String apellido;
+    private String numeroTelefono;
+    private String mail;
+    public Usuario(String usuario, String contrasenia, String nombre, String apellido, String numeroTelefono, String mail) {
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numeroTelefono = numeroTelefono;
+        this.mail = mail;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
