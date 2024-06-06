759e61f96568eb5b98e4028880cdbe0dc5bad8da
JuanmaVD <JuanmaVD21@gmail.com>
2024-06-06 11:20:07 -0300

    1

--- src/test/java/usuarioTest.java
+++ src/test/java/usuarioTest.java
@@ -11,5 +11,11 @@ public class usuarioTest {
         Usuario usuario = new Usuario();
         assertTrue("El nombre de usuario no debe de contener numeros",usuario.esNombreValido("Lucio"));
     }
-    
+    @Test
+    public void testEsNombreValido_ConNumeros() {
+        Usuario usuario = new Usuario();
+        String nombre = "Juan123";
+        assertFalse(usuario.esNombreValido(nombre));
+    }
 }
+
