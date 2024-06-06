576b87d8a29e1aed42cd6de8a242486e04006eaf
JuanmaVD <JuanmaVD21@gmail.com>
2024-06-06 11:22:04 -0300

    fixcommit

--- src/test/java/usuarioTest.java
+++ src/test/java/usuarioTest.java
@@ -6,11 +6,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class usuarioTest {
+
+    //Validar que si envio un nombre sin numeros esta correcto
     @Test
     public void validarNombreUsuarioSinNumerosTrue(){
         Usuario usuario = new Usuario();
         assertTrue("El nombre de usuario no debe de contener numeros",usuario.esNombreValido("Lucio"));
     }
+
+    //Validar que si mando un nombre con numeros me tira el error
     @Test
     public void testEsNombreValido_ConNumeros() {
         Usuario usuario = new Usuario();
