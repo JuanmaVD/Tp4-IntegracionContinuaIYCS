e7990f8dce43e170275989d3f92e720f240a7fc4
JuanmaVD <JuanmaVD21@gmail.com>
2024-06-08 10:59:13 -0300

    Revert "3"
    This reverts commit b0c6b741d9608e5da6ecf398c97a8bb602cba742.

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,64 +1,62 @@
 package test.java;
 
 import main.java.Usuario;
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
 import static org.testng.AssertJUnit.assertFalse;
 import static org.testng.AssertJUnit.assertTrue;
 
 public class Test {
 
     //Validar que si envio un nombre sin numeros esta correcto
-    @Test
+    @org.testng.annotations.Test
     public void validarNombreUsuarioSinNumerosTrue(){
         Usuario usuario = new Usuario();
         assertTrue(usuario.esNombreValido("Lucio"));
     }
 
     //Validar que si mando un nombre con numeros me tira el error
-    @Test
+
+    @org.testng.annotations.Test
     public void testEsNombreValido_ConNumeros() {
         Usuario usuario = new Usuario();
         assertFalse(usuario.esNombreValido("Juan123"));
     }
 
     //Validar que teléfono debe contener solo números y tener entre 10 y 15 dígitos
-    @Test
+    @org.testng.annotations.Test
     public void validarNumeroTelefonoSoloNumeros(){
         Usuario usuario = new Usuario();
         assertTrue(usuario.esTelefonoValido("2612194702"));
     }
 
     //Validar que si el telefono tiene letras tire el error
-    @Test
+    @org.testng.annotations.Test
     public void validarNumeroTelefonoNumerosYletras(){
         Usuario usuario = new Usuario();
         assertFalse(usuario.esTelefonoValido("261219470qwerty"));
     }
 
     //Validar TelefonoMenos10Caracteres tire error
-    @Test
+    @org.testng.annotations.Test
     public void validarNumeroTelefonoMenos10Caracteres(){
         Usuario usuario = new Usuario();
         assertFalse(usuario.esTelefonoValido("261219470"));
     }
 
     //Validar TelefonoMas15Caracteres tire error
-    @Test
+    @org.testng.annotations.Test
     public void validarNumeroTelefonoMas15Caracteres(){
         Usuario usuario = new Usuario();
         assertFalse(usuario.esTelefonoValido("2612194701234567"));
     }
 
     //"El mail debe tener A-Z  @ A-Z ."
-    @Test
+    @org.testng.annotations.Test
     public void mailTieneFormatoAdecuado(){
         Usuario usuario = new Usuario();
         assertTrue(usuario.esEmailValido("boca@boca"));
     }
     //"El mail debe tener A-Z  @ A-Z "
-    @Test
+    @org.testng.annotations.Test
     public void mailTieneFormatoInadecuado(){
         Usuario usuario = new Usuario();
         assertFalse("El mail debe tener A-Z  @ A-Z ", usuario.esEmailValido("boca"));
@@ -66,13 +64,13 @@ public class Test {
     }
 
     //"El mail no debe estar vacio .",
-    @Test
+    @org.testng.annotations.Test
     public void esMailVacio(){
         Usuario usuario = new Usuario();
         assertTrue( usuario.esMailVacio(""));
     }
     //Validar que el apellido contenga mayuscula inicial
-    @Test
+    @org.testng.annotations.Test
     public void validarApellidoMayusculaInicial(){
         Usuario usuario = new Usuario();
         String apellido = "Martinez";
@@ -80,7 +78,7 @@ public class Test {
     }
 
     //validar si la contraseña tiene 8 caracteres
-    @Test
+    @org.testng.annotations.Test
     public void validarLongitudContraseniaCorrecta(){
         Usuario usuario = new Usuario();
         String contrasenia = "12345678";
@@ -88,7 +86,7 @@ public class Test {
     }
 
     //validar si la contraseña no tiene 8 caracteres
-    @Test
+    @org.testng.annotations.Test
     public void validarLongitudContraseniaIncorrecta(){
         Usuario usuario = new Usuario();
         String contrasenia = "1234567";
